07b2e8189b383c4d28c80d63b5fba6c395de87c1
Erlend Dahl <erlend.dahl@sintef.no>
2019-05-10 12:24:08 +0200

    Renamed the app to JetReader.

--- EbookReader/EbookReader.UWP/Properties/AssemblyInfo.cs
+++ EbookReader/EbookReader.UWP/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EbookReader.UWP")]
+[assembly: AssemblyTitle("JetReader")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EbookReader.UWP")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("JetReader")]
+[assembly: AssemblyCopyright("Copyright © Erlend Dahl")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
