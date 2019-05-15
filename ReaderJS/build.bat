8f150bfcda1a3c1c92907944f99f9214edd82a0d
Erlend Dahl <erlend.dahl@sintef.no>
2019-05-15 14:11:40 +0200

    Removed build file. It was easier to make a symlink from a folder without special letters.

--- ReaderJS/build.bat
+++
@@ -1,8 +0,0 @@
-robocopy "C:\Users\erlendd\Dropbox\Mine program\C#\JetReader\ReaderJS\Src" "C:\Code\ReaderJs\Src" /E /is /it > nul
-
-cd "C:\Code\ReaderJs\Src\"
-C:
-
-call grunt build
-
-robocopy "C:\Code\ReaderJs\Build" "C:\Users\erlendd\Dropbox\Mine program\C#\JetReader\ReaderJS\Build" /E /is /it > nul
