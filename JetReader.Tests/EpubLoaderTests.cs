c9679709bba600415005c598bf010fbbbb2eb095
Erlend Dahl <erlend.dahl@sintef.no>
2019-05-22 13:01:37 +0200

    Created tests for Epub loading. Shows that the current implementation is very slow, and fails on approximately 100 of 400 epubs.

---
+++ JetReader.Tests/EpubLoaderTests.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using EbookReader;
+using EbookReader.BookLoaders.Epub;
+using EbookReader.Service;
+using EpubSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JetReader.Tests
+{
+    [TestClass]
+    public class EpubLoaderTests
+    {
+        //private string _libraryPath = @"C:\Users\erlendd\Dropbox\Bøker\Calibre-bibliotek";
+        private string _libraryPath = @"C:\Users\Erlend\Dropbox\Bøker\Calibre-bibliotek";
+
+        public static string GetMd5(byte[] bytes)
+        {
+            using (var md5 = System.Security.Cryptography.MD5.Create())
+            {
+                var hashBytes = md5.ComputeHash(bytes);
+
+                var sb = new StringBuilder();
+                for (var i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString();
+            }
+        }
+
+        internal static byte[] ReadFully(Stream input)
+        {
+            using (var ms = new MemoryStream())
+            {
+                input.CopyTo(ms);
+                return ms.ToArray();
+            }
+        }
+
+        [TestMethod]
+        public void TestAllEpubs()
+        {
+            //TODO: Compare to NuGet libraries
+            //Successes: 310 / 408
+            //Time usage: 112,7570463 seconds
+
+            var epubs = Directory.GetFiles(_libraryPath, "*.epub", SearchOption.AllDirectories);
+            var failures = new List<string>();
+
+            IocManager.Build();
+
+            var fs = new FileService();
+            var loader = new EpubLoader(fs);
+            var start = DateTime.Now;
+
+            foreach (var file in epubs)
+            {
+                byte[] data;
+                using (var r = File.OpenRead(file))
+                {
+                    data = ReadFully(r);
+                }
+                var id = GetMd5(data);
+
+                try
+                {
+                    loader.OpenBook(file).Wait();
+                }
+                catch (Exception ex)
+                {
+                    failures.Add(Path.GetDirectoryName(file) + ": " + ex.Message + (string.Join("; ", (ex as AggregateException)?.InnerExceptions.Select(p => p.Message))));
+                }
+            }
+
+            Console.WriteLine("Successes: " + (epubs.Length - failures.Count) + " / " + epubs.Length);
+            Console.WriteLine("Time usage: " + DateTime.Now.Subtract(start).TotalSeconds + " seconds");
+
+            foreach (var failure in failures)
+                Console.WriteLine(failure);
+        }
+
+        [TestMethod]
+        public void TestAllEpubsEpubsharp()
+        {
+            //TODO: Compare to NuGet libraries
+            //Successes: 310 / 408
+            //Time usage: 112,7570463 seconds
+
+            var epubs = Directory.GetFiles(_libraryPath, "*.epub", SearchOption.AllDirectories);
+            var failures = new List<string>();
+            var start = DateTime.Now;
+
+            foreach (var file in epubs)
+            {
+                byte[] data;
+                using (var r = File.OpenRead(file))
+                {
+                    data = ReadFully(r);
+                }
+                var id = GetMd5(data);
+
+                try
+                {
+                    var book = EpubReader.Read(file);
+                    Debug.WriteLine(book.Title);
+                }
+                catch (Exception ex)
+                {
+                    failures.Add(Path.GetDirectoryName(file) + ": " + ex.Message + (string.Join("; ", ((ex as AggregateException)?.InnerExceptions?.Select(p => p.Message)) ?? new string[0])));
+                }
+            }
+
+            Console.WriteLine("Successes: " + (epubs.Length - failures.Count) + " / " + epubs.Length);
+            Console.WriteLine("Time usage: " + DateTime.Now.Subtract(start).TotalSeconds + " seconds");
+
+            foreach (var failure in failures)
+                Console.WriteLine(failure);
+        }
+
+        [TestMethod]
+        public void TestAllEpubsVersOne()
+        {
+            //TODO: Compare to NuGet libraries
+            //Successes: 310 / 408
+            //Time usage: 112,7570463 seconds
+
+            var epubs = Directory.GetFiles(_libraryPath, "*.epub", SearchOption.AllDirectories);
+            var failures = new List<string>();
+            var start = DateTime.Now;
+
+            foreach (var file in epubs)
+            {
+                byte[] data;
+                using (var r = File.OpenRead(file))
+                {
+                    data = ReadFully(r);
+                }
+                var id = GetMd5(data);
+
+                try
+                {
+                    var book = VersOne.Epub.EpubReader.ReadBook(file);
+                    Debug.WriteLine(book.Schema.Package);
+                }
+                catch (Exception ex)
+                {
+                    failures.Add(Path.GetDirectoryName(file) + ": " + ex.Message + (string.Join("; ", ((ex as AggregateException)?.InnerExceptions?.Select(p => p.Message)) ?? new string[0])));
+                }
+            }
+
+            Console.WriteLine("Successes: " + (epubs.Length - failures.Count) + " / " + epubs.Length);
+            Console.WriteLine("Time usage: " + DateTime.Now.Subtract(start).TotalSeconds + " seconds");
+
+            foreach (var failure in failures)
+                Console.WriteLine(failure);
+        }
+    }
+}
---
+++ JetReader.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JetReader.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JetReader.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("834df7dd-6b85-4e66-b912-c5255e0cc3ed")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
