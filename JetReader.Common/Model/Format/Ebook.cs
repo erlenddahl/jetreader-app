31394f92205f2c15c1e8851e979a92b3153ce22d
Erlend Dahl <erlend.dahl@gmail.com>
2020-05-11 22:05:03 +0200

    Made the info popup nicer.

--- JetReader.Common/Model/Format/Ebook.cs
+++ JetReader.Common/Model/Format/Ebook.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
+using Extensions.TimeSpanExtensions;
 
 namespace JetReader.Model.Format
 {
@@ -52,11 +53,13 @@ namespace JetReader.Model.Format
         {
             await Info.WaitForProcessingToFinish();
 
+            var secondsRead = Info.ReadStats.Dates.Sum(p => p.Seconds);
+            var wordsRead = Info.ReadStats.Dates.Sum(p => p.Words);
+            var totalWords = Info.ChapterInfo.Sum(p => p.Words);
+            var wordsPerSecond = wordsRead / secondsRead;
+
             return new List<(string title, string value)>()
             {
-                ("Title", Title),
-                ("Author", Author),
-                ("Language", Language),
                 ("Description", Description),
                 ("", ""),
 
@@ -64,8 +67,14 @@ namespace JetReader.Model.Format
                 ("Book file size", (Info.BookFileSize / 1000d).ToString("n0") + " kB"),
                 ("", ""),
 
-                ("Words in book", Info.ChapterInfo.Sum(p => p.Words).ToString("n0")),
-                ("Characters in book", Info.ChapterInfo.Sum(p => p.Letters).ToString("n0"))
+                ("Words in book", totalWords.ToString("n0")),
+                ("Characters in book", Info.ChapterInfo.Sum(p => p.Letters).ToString("n0")),
+                ("", ""),
+
+                ("Total reading time", TimeSpan.FromSeconds(secondsRead).ToShortPrettyFormat()),
+                ("Total words read", $"{wordsRead:n0} ({(wordsRead / totalWords * 100):n1}%)"),
+                ("Average reading speed", $"{(wordsPerSecond * 60):n0} words/min"),
+                ("Estimated remaining reading time", TimeSpan.FromSeconds((totalWords - wordsRead)/wordsPerSecond).ToShortPrettyFormat()),
             };
         }
     }
--- JetReader.Common/Page/Reader/Popups/BookInfoPopup.xaml.cs
+++ JetReader.Common/Page/Reader/Popups/BookInfoPopup.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Extensions.TimeSpanExtensions;
 using JetReader.Books;
+using JetReader.Extensions;
 using JetReader.Model.Format;
 using Rg.Plugins.Popup.Services;
 using Xamarin.Forms;
@@ -11,38 +15,64 @@ namespace JetReader.Page.Reader.Popups
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class BookInfoPopup : Rg.Plugins.Popup.Pages.PopupPage
     {
+        public Ebook Book { get; }
+
         public BookInfoPopup(Ebook book)
         {
+            Book = book;
             BindingContext = this;
 
             InitializeComponent();
 
-            LoadImage(book);
-            
-            foreach (var item in book.GetInfo().Result)
+            DisplayInfo();
+        }
+
+        private async void DisplayInfo()
+        {
+            LoadImage(Book);
+
+            await Book.Info.WaitForProcessingToFinish();
+
+            foreach (var item in Book.GetInfo().Result)
             {
-                if (string.IsNullOrWhiteSpace(item.title))
-                {
-                    container.Children.Add(new Label());
-                }
-                else
-                {
-                    if (string.IsNullOrWhiteSpace(item.value)) continue;
-                    var box = new StackLayout() { Orientation = StackOrientation.Horizontal };
-                    box.Children.Add(new Label()
-                    {
-                        FormattedText = new FormattedString()
-                        {
-                            Spans =
-                            {
-                                new Span(){ Text = item.title + ": ", FontAttributes = FontAttributes.Bold, FontSize = 16},
-                                new Span(){ Text = item.value, FontSize = 16 }
-                            }
-                        }
-                    });
-                    container.Children.Add(box);
-                }
+                AddInfoControl(container, item);
             }
+
+            AddEmpty(container);
+            foreach (var item in Book.Info.ReadStats.Dates.Select(p => (p.Date.ToShortDateString(), $"{TimeSpan.FromSeconds(p.Seconds).ToShortPrettyFormat()}, {p.Words:n0} words, {(p.Words / (p.Seconds / 60)):n0} words/min.")))
+            {
+                AddInfoControl(container, item);
+            }
+        }
+
+        private void AddInfoControl(StackLayout parent, (string title, string value) item, double marginLeft = 0)
+        {
+            if (string.IsNullOrWhiteSpace(item.title))
+            {
+                AddEmpty(parent);
+            }
+            else
+            {
+                if (string.IsNullOrWhiteSpace(item.value)) return;
+                var l = new Label()
+                {
+                    FormattedText = new FormattedString()
+                    {
+                        Spans =
+                        {
+                            new Span() {Text = item.title + ": ", FontAttributes = FontAttributes.Bold, FontSize = 16},
+                            new Span() {Text = item.value, FontSize = 16}
+                        }
+                    }
+                };
+                l.Margin = new Thickness(marginLeft, l.Margin.Top, l.Margin.Right, l.Margin.Bottom);
+                parent.Children.Add(l);
+            }
+        }
+
+        private void AddEmpty(StackLayout parent)
+        {
+            parent.Children.Add(new Label());
         }
 
         private void LoadImage(Ebook book)
@@ -51,6 +81,11 @@ namespace JetReader.Page.Reader.Popups
 
             Cover.Source = ImageSource.FromFile(book.Info.GetTempPath(book.CoverFilename));
             Cover.Aspect = Aspect.AspectFit;
+        }
+
+        private void CloseButton_OnClicked(object sender, EventArgs e)
+        {
+            this.Hide();
         }
     }
 }
